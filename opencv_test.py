c61f800d36f539ae27b2d0f6273ce4d7faa5a823
naomichoy <41099656+naomichoy@users.noreply.github.com>
2021-03-23 21:58:41 +0800

    Update opencv_test.py

--- opencv_test.py
+++ opencv_test.py
@@ -1,6 +1,34 @@
 import cv2
+
+def gstreamer_pipeline(
+    capture_width=1280,
+    capture_height=720,
+    display_width=1280,
+    display_height=720,
+    framerate=60,
+    flip_method=0,
+):
+    return (
+        "nvarguscamerasrc ! "
+        "video/x-raw(memory:NVMM), "
+        "width=(int)%d, height=(int)%d, "
+        "format=(string)NV12, framerate=(fraction)%d/1 ! "
+        "nvvidconv flip-method=%d ! "
+        "video/x-raw, width=(int)%d, height=(int)%d, format=(string)BGRx ! "
+        "videoconvert ! "
+        "video/x-raw, format=(string)BGR ! appsink"
+        % (
+            capture_width,
+            capture_height,
+            framerate,
+            flip_method,
+            display_width,
+            display_height,
+        )
+    )
+
 # cap = cv2.VideoCapture(0,cv2.CAP_V4L2)
-cap = cv2.VideoCapture(0,cv2.CAP_GSTREAMER) 
+cap = cv2.VideoCapture(gstreamer_pipeline(flip_method=0),cv2.CAP_GSTREAMER) 
 
 while (True):
   ret, frame = cap.read()
@@ -13,3 +41,4 @@ while (True):
       break
 
 cap.release()
+cv2.destroyAllWindows()
