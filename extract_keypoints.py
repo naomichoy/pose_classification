31b20b9ad7506f474f1f25d3d611cb4355fad6d0
naomichoy <41099656+naomichoy@users.noreply.github.com>
2021-03-23 22:02:35 +0800

    Update extract_keypoints.py

--- extract_keypoints.py
+++ extract_keypoints.py
@@ -14,6 +14,34 @@ import argparse
 import os.path
 
 
+def gstreamer_pipeline(
+    capture_width=1280,
+    capture_height=720,
+    display_width=1280,
+    display_height=720,
+    framerate=60,
+    flip_method=0,
+):
+    return (
+        "nvarguscamerasrc ! "
+        "video/x-raw(memory:NVMM), "
+        "width=(int)%d, height=(int)%d, "
+        "format=(string)NV12, framerate=(fraction)%d/1 ! "
+        "nvvidconv flip-method=%d ! "
+        "video/x-raw, width=(int)%d, height=(int)%d, format=(string)BGRx ! "
+        "videoconvert ! "
+        "video/x-raw, format=(string)BGR ! appsink"
+        % (
+            capture_width,
+            capture_height,
+            framerate,
+            flip_method,
+            display_width,
+            display_height,
+        )
+    )
+
+
 '''
 hnum: 0 based human index
 kpoint : keypoints (float type range : 0.0 ~ 1.0 ==> later multiply by image width, height
@@ -123,7 +151,8 @@ def execute(img, t):
 
 
 
-cap = cv2.VideoCapture(0)
+# cap = cv2.VideoCapture(0)
+cap = cv2.VideoCapture(gstreamer_pipeline(flip_method=0),cv2.CAP_GSTREAMER) 
 cap.set(cv2.CAP_PROP_FRAME_WIDTH, 640)
 cap.set(cv2.CAP_PROP_FRAME_HEIGHT, 480)
 
