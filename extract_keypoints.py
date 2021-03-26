8a7afff08b148df5b268b3ade46b002e46ea8b45
naomichoy <41099656+naomichoy@users.noreply.github.com>
2021-03-26 17:51:23 +0800

    draw keypoint on frame

--- extract_keypoints.py
+++ extract_keypoints.py
@@ -159,16 +159,16 @@ def execute(img, t):
     #     print("Human index:%d "%( i ))
     #     keypoints = get_keypoint(objects, i, peaks)
     #     print(keypoints[0])
-    #     for j in range(len(keypoints)):
-    #         print(keypoints[j])
-    #         if keypoints[j][1]:
-    #             x = round(keypoints[j][2] * WIDTH )
-    #             y = round(keypoints[j][1] * HEIGHT)
-    #     #         x = round(keypoints[j][2] * WIDTH * X_compress) # incorrect adjustment, compress factor not needed
-    #     #         y = round(keypoints[j][1] * HEIGHT * Y_compress)
-    #             cv2.circle(img, (x, y), 3, color, 2)
-    #             cv2.putText(img , "%d" % int(keypoints[j][0]), (x + 5, y),  cv2.FONT_HERSHEY_SIMPLEX, 1, (0, 255, 255), 1)
-    #             cv2.circle(img, (x, y), 3, color, 2)
+        for j in range(len(keypoints)):
+            print(keypoints[j])
+            if keypoints[j][1]:
+                x = round(keypoints[j][2] * WIDTH )
+                y = round(keypoints[j][1] * HEIGHT)
+        #         x = round(keypoints[j][2] * WIDTH * X_compress) # incorrect adjustment, compress factor not needed
+        #         y = round(keypoints[j][1] * HEIGHT * Y_compress)
+                cv2.circle(img, (x, y), 3, color, 2)
+                cv2.putText(img , "%d" % int(keypoints[j][0]), (x + 5, y),  cv2.FONT_HERSHEY_SIMPLEX, 1, (0, 255, 255), 1)
+                cv2.circle(img, (x, y), 3, color, 2)
 
     
     # draw_objects(img, counts, objects, peaks)
