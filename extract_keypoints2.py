8fbd9689342e73a9491e00832d97c277eaab47f9
naomichoy <41099656+naomichoy@users.noreply.github.com>
2021-04-08 14:20:20 +0800

    Update extract_keypoints2.py

--- extract_keypoints2.py
+++ extract_keypoints2.py
@@ -147,10 +147,10 @@ def execute(img, t):
             Lshoulder = keypoints[5]
             Rshoulder = keypoints[6]
         
-            print(head)
+            # print(head)
             if head[1]: 
                 head_y = head[1] * HEIGHT # index 1 = height
-                print(head_y)
+                # print(head_y)
                 head_y_list.append(head_y)
 
 
