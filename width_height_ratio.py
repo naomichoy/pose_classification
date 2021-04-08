6400c74b9c35429cfc843d2d1c14a793c099c5fc
naomichoy <41099656+naomichoy@users.noreply.github.com>
2021-04-08 20:48:44 +0800

    Update width_height_ratio.py

--- width_height_ratio.py
+++ width_height_ratio.py
@@ -201,10 +201,10 @@ def execute(img, t):
             y_min = min(keypoints_y)
             x_max = max(keypoints_x)
             x_min = min(keypoints_x)
-            cv2.line(image, (x_max, x_max), (y_max,y_min), (255, 0, 0), 1)
-            cv2.line(image, (x_min, y_min), (x_max,y_min), (255, 0, 0), 1)
-            cv2.line(image, (x_min, x_min), (y_min,y_max), (255, 0, 0), 1)
-            cv2.line(image, (x_min, y_max), (x_max,y_max), (255, 0, 0), 1)
+            cv2.line(img, (x_max, x_max), (y_max,y_min), (255, 0, 0), 1)
+            cv2.line(img, (x_min, y_min), (x_max,y_min), (255, 0, 0), 1)
+            cv2.line(img, (x_min, x_min), (y_min,y_max), (255, 0, 0), 1)
+            cv2.line(img, (x_min, y_max), (x_max,y_max), (255, 0, 0), 1)
             body_ratio = (y_max-y_min)/(x_max-x_min)
             if body_ratio < 1:
                 isFallRatio = True
